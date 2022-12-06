ca3dcb03de0db07fc3d24a683d1d9e19af130ebf
Kovács Tibor <ifjutibor@gmail.com>
2022-12-06 22:25:55 +0100

    feat(metrics): add performance metrics with builtin reporting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,3 +4,7 @@ import { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
